af33af7a93c39dc7a2780ab867834f89ff8f4e00
pavan <Pavanpk123@gmail.com>
2022-09-01 18:56:48 +0530

    variables

---
+++ index.js
@@ -0,0 +1,25 @@
+const person = {
+    name :"pavan",
+    age : "22",
+    gender : "male"
+}
+
+let name = person.name;
+let age = person.age;
+let gender = person.gender;
+
+console.log(name);
+console.log(age);
+console.log(gender)
+
+//assigning object attributes to variables
+const person = {
+    name: "pavan",
+    age:"22",
+    gender:"male"   
+}
+
+let {name,age,gender} = person
+console.log(name);
+console.log(age);
+console.log(gender);
